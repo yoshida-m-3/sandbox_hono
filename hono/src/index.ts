11420c4f6f935b0a6c8bf248297b8bef1687057d
Makoto Yoshida <milmed1013@gmail.com>
2024-10-19 22:52:31 +0900

    add validation sample code

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,9 +1,27 @@
-import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get(
+  "/test/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.string().regex(/^\d+$/).transform(Number),
+    })
+  ),
+  zValidator("query", z.object({ name: z.string().optional() })),
+  (c) => {
+    const { id } = c.req.valid("param");
+    const { name } = c.req.valid("query");
+    return c.json({ id, name });
+  }
+);
+
+export default app;

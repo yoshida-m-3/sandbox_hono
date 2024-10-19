a8e28f56f4414bf9b551a4f22fbf306de132db01
Makoto Yoshida <milmed1013@gmail.com>
2024-10-19 11:13:11 +0900

    feat: sample pattern

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,16 +1,51 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { View } from "./view";
+import { basicAuth } from "hono/basic-auth";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/admin/*",
+  basicAuth({
+    username: "admin",
+    password: "secret",
+  })
+);
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/admin", (c) => {
+  return c.text("You are authorized!");
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "Hello Hono!",
+  });
+});
+
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
+
+app.post("/posts", (c) => c.text("Created!", 201));
+app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
+
+app.get("/page", (c) => {
+  return c.html(View());
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});

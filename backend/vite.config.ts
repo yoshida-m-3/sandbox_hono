92d611e4db3eda41214f88d10aaf04f970888f1c
Makoto Yoshida <milmed1013@gmail.com>
2024-10-22 23:09:08 +0900

    add package - @hono/vite-build $ pnpm add -D @hono/vite-dev-server

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import devServer from "@hono/vite-dev-server";
+import build from "@hono/vite-build/node";
 
 export default defineConfig({
   plugins: [
     devServer({
       entry: "src/index.ts", // The file path of your application.
+    }),
+    build({
+      // Defaults are `src/index.ts`,`./src/index.tsx`,`./app/server.ts`
+      entry: "./src/index.ts",
     }),
   ],
 });

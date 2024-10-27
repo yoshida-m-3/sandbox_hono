7bb391235d153a10253ac2ec7b4f34cfdab38f32
Makoto Yoshida <milmed1013@gmail.com>
2024-10-27 17:16:50 +0900

    apply format

--- apps/backend/src/infrastructures/supabase/supabase-repository.ts
+++ apps/backend/src/infrastructures/supabase/supabase-repository.ts
@@ -1,16 +1,16 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { env } from "@/env";
+import { type SupabaseClient, createClient } from "@supabase/supabase-js";
 
 export class SupabaseRepository {
-  private supabase: SupabaseClient = createClient(
-    env.VITE_SUPABASE_URL,
-    env.VITE_SUPABASE_KEY
-  );
+	private supabase: SupabaseClient = createClient(
+		env.VITE_SUPABASE_URL,
+		env.VITE_SUPABASE_KEY,
+	);
 
-  async signUp(email: string, password: string) {
-    const { data, error } = await this.supabase.auth.signUp({
-      email,
-      password,
-    });
-  }
+	async signUp(email: string, password: string) {
+		const { data, error } = await this.supabase.auth.signUp({
+			email,
+			password,
+		});
+	}
 }
--- apps/backend/vite.config.ts
+++ apps/backend/vite.config.ts
@@ -3,16 +3,16 @@ import devServer from "@hono/vite-dev-server";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    devServer({
-      entry: "src/index.ts", // The file path of your application.
-    }),
-    build({
-      // Defaults are `src/index.ts`,`./src/index.tsx`,`./app/server.ts`
-      entry: "./src/index.ts",
-    }),
-  ],
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		devServer({
+			entry: "src/index.ts", // The file path of your application.
+		}),
+		build({
+			// Defaults are `src/index.ts`,`./src/index.tsx`,`./app/server.ts`
+			entry: "./src/index.ts",
+		}),
+	],
 });
